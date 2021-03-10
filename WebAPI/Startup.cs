ba9f175da167fdabe7060431a54d2e80380f8366
Koray HALAT <korayhalat79@gmail.com>
2021-03-11 01:07:25 +0300

    koray_halat

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -45,8 +45,8 @@ namespace WebAPI
             //services.AddSingleton<IProductService,ProductManager>();
             //services.AddSingleton<IProductDal, EfProductDal>();
 
-            
 
+            services.AddCors();
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -77,6 +77,7 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseCors(builder=>builder.WithOrigins("http://localhost:49340").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
